425ed9f72d98457a5c6c54824d3069ca180142c9
M Iqbal Adenan <kumpultugas17@gmail.com>
2021-10-13 14:13:55 +0800

    Membuat Halaman Home

---
+++ pertemuan10/home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Kategori</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Perpus</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kategori.php">Kategori</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="buku.php">Buku</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="row mt-3">
+            <div class="col-md-3">
+                <div class="text-center">
+                    <img src="images/lspinfindo.png" class="img-thumbnail rounded" alt="lspinfindo">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <h2>Selamat Datang!</h2>
+                <h3>Aplikasi Perpustakaan 2021</h3>
+                <h6>M. Iqbal Adenan</h6>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
